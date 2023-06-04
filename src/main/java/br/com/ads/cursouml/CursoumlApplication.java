cb0c9ac9086b6835b4766572a5d505281be1d86f
Adson Conceição <adsonconcecao@yahoo.com.br>
2023-06-03 22:35:20 -0300

    Produtos de associação de muitos para muitos (ManyToMany)

--- src/main/java/br/com/ads/cursouml/CursoumlApplication.java
+++ src/main/java/br/com/ads/cursouml/CursoumlApplication.java
@@ -1,5 +1,6 @@
 package br.com.ads.cursouml;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.ads.cursouml.domain.Categoria;
+import br.com.ads.cursouml.domain.Produto;
 import br.com.ads.cursouml.repositories.CategoriaRepository;
+import br.com.ads.cursouml.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoumlApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoumlApplication.class, args);
@@ -23,10 +29,22 @@ public class CursoumlApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat1 = new Categoria(null, "Informatica", new ArrayList<Produto>());
+		Categoria cat2 = new Categoria(null, "Escritório", new ArrayList<Produto>());
+		
+		Produto p1 = new Produto(null, "Computator", 2000.00, new ArrayList<Categoria>());
+		Produto p2 = new Produto(null, "Impressora", 800.00, new ArrayList<Categoria>());
+		Produto p3 = new Produto(null, "Mouse", 80.00, new ArrayList<Categoria>());
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList( p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2,p3));
 		
 	}
 
--- src/main/java/br/com/ads/cursouml/domain/Produto.java
+++ src/main/java/br/com/ads/cursouml/domain/Produto.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.AllArgsConstructor;
@@ -22,19 +24,24 @@ import lombok.Setter;
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
-	
-	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
-	
-	
+	private Double preco;
 
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+	           joinColumns = @JoinColumn(name = "produto_id"),
+	           inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	           )
+	private List<Categoria> categorias = new ArrayList<>();
+	
+	
+	
 }
+
